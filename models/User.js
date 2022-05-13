e34e9b816b5328ec846e480a862e341266260fdd
SAB <sevanboyadjian@gmail.com>
2022-05-13 10:59:10 -0400

    Added Models

--- models/User.js
+++ models/User.js
@@ -1,4 +1,44 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const { Schema, model } = require('mongoose');
 
-module.exports = { User, Thought };
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i, "Please enter a valid email"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
